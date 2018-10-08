87c8471bd84c5fa1e615e08ff60cf8b17020939f
Emo <eiv.razgrad@gmail.com>
2018-10-08 14:28:53 +0300

    Add first and fourth

---
+++ HitList/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HitList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var people = new Dictionary<string, Dictionary<string, string>>();
+            var infIndex = int.Parse(Console.ReadLine());
+            string command = Console.ReadLine();
+
+
+            while (command != "end transmissions")
+            {
+
+                var tokens = command.Split("=".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                var name = tokens[0];
+
+                if (!people.ContainsKey(name))
+                {
+                    people[name] = new Dictionary<string, string>();
+
+                }
+
+                tokens = tokens[1].Split(";".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var part in tokens)
+                {
+                    var temp = part.Split(":".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+                    people[name][temp[0]] = temp[1];
+                }
+
+                command = Console.ReadLine();
+            }
+            var targetMan = Console.ReadLine().Split()[1];
+            Console.WriteLine($"Info on {targetMan}:");
+            var targetManInfo = people[targetMan].ToArray();
+            foreach (var target in targetManInfo.OrderBy(x => x.Key))
+            {
+                Console.WriteLine("---"+target.Key+": "+target.Value);
+            }
+            var infInd = people[targetMan].Select(x => x.Key.Length + x.Value.Length).Sum();
+            Console.WriteLine("Info index: " + infInd);
+            if (infInd >= infIndex)
+            {
+                Console.WriteLine("Proceed");
+            }
+            else
+            {
+                Console.WriteLine($"Need {infIndex-infInd} more info.");
+            }
+          
+        }
+    }
+}
---
+++ KeyRevolver/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KeyRevolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int priceEachBullet = int.Parse(Console.ReadLine());
+            int sizeBulletBarel = int.Parse(Console.ReadLine());
+
+            int[] bulletArr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            Stack<int> bullets = new Stack<int>(bulletArr);
+            int[] lockArr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            Queue<int> locks = new Queue<int>(lockArr);
+            int counter = 0;
+
+            int valueOfInteligence = int.Parse(Console.ReadLine());
+
+            while (locks.Count > 0 && bullets.Count > 0)
+            {
+                var bullet = bullets.Pop();
+
+                if (bullet<=locks.Peek())
+                {
+                    Console.WriteLine("Bang!");
+                    locks.Dequeue();
+                }
+                else
+                {
+                    Console.WriteLine("Ping!");
+                }
+                counter++;
+
+                if (counter % sizeBulletBarel == 0 && bullets.Count > 0)
+                {
+                    Console.WriteLine("Reloading!");
+                }
+            }
+            if (locks.Count == 0)
+            {
+                var bulletsCost = Math.Abs((bulletArr.Count() - bullets.Count)*priceEachBullet - valueOfInteligence);
+
+                Console.WriteLine($"{bullets.Count} bullets left. Earned ${bulletsCost}");
+            }
+            else
+            {
+                Console.WriteLine($"Couldn't get through. Locks left: {locks.Count}");
+            }
+
+
+        }
+    }
+}
